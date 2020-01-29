6cc90b8b69e5dfb2ee2651f859264c5f8e7798bd
Rogério Júnior <junior.rogerio8@gmail.com>
2020-01-29 05:22:43 -0200

    Encode method and standart of message input and output

--- src/functions.js
+++ src/functions.js
@@ -1,19 +1,5 @@
-let input = [
-  0xc6,
-  0x57,
-  0x54,
-  0x95,
-  0x5e,
-  0x9e,
-  0x6b,
-  0xc6,
-  0x55,
-  0x17,
-  0x55,
-  0x52,
-  0x9e,
-  0x21
-];
+let input =
+  "0xc6 0x57 0x54 0x95 0x5e 0x9e 0x6b 0xc6 0x55 0x17 0x55 0x52 0x9e 0x21";
 
 const tableX = [
   "11110",
@@ -37,13 +23,17 @@ const tableX = [
 function decodeMessage(encondeMessage, cypher) {
   let binary = [];
 
+  encondeMessage = encondeMessage
+    .split(" ")
+    .map(element => parseInt(element, 16));
+
   for (let i = 0; i < encondeMessage.length; i++) {
     let aux = "";
 
-    if (encondeMessage[i] === 198) {
+    if (encondeMessage[i] === 0xc6) {
       i++;
 
-      while (encondeMessage[i] != 107 && encondeMessage[i] != 33) {
+      while (encondeMessage[i] != 0x6b && encondeMessage[i] != 0x21) {
         let convert = encondeMessage[i].toString(2);
 
         aux += "00000000".substring(convert.length) + convert;
@@ -88,4 +78,67 @@ function decodeMessage(encondeMessage, cypher) {
   return message;
 }
 
-console.log("[" + decodeMessage(input, tableX) + "]");
+function alterMessage(message) {
+  let aux = "";
+
+  for (let i = 0; i < message.length; i++) {
+    aux += i & 1 ? message[i].toLowerCase() : message[i];
+  }
+
+  message = message
+    .replace(message, aux)
+    .replace(/ /g, "_")
+    .split("")
+    .reverse()
+    .join("");
+
+  return message;
+}
+
+function encodeMessage(message, cypher) {
+  while (message.length & 3) message += "_";
+
+  let binary = "";
+
+  for (const char of message) {
+    let aux = char.charCodeAt(0).toString(2);
+
+    binary += "00000000".substring(aux.length) + aux;
+  }
+
+  let aux = "",
+    j = 0;
+
+  while (j < binary.length) {
+    aux += cypher[parseInt(binary.substring(j, j + 4), 2)];
+    j += 4;
+  }
+
+  binary = binary.replace(binary, aux);
+
+  let hexadecimal = [];
+  j = 0;
+
+  while (j < binary.length) {
+    hexadecimal.push(parseInt(binary.substring(j, j + 8), 2));
+    j += 8;
+  }
+
+  hexadecimal = hexadecimal.map(element => element ^ 0x13);
+
+  for (let i = 0; i < hexadecimal.length; i += 7) {
+    hexadecimal.splice(i, 0, 0xc6);
+    hexadecimal.splice(i + 6, 0, i + 7 < hexadecimal.length ? 0x6b : 0x21);
+  }
+
+  message = hexadecimal
+    .map(
+      element =>
+        (element < 16 ? "0x0" : "0x") + element.toString(16).toUpperCase()
+    )
+    .join(" ");
+
+  return message;
+}
+
+console.log(encodeMessage(alterMessage(decodeMessage(input, tableX)), tableX));
